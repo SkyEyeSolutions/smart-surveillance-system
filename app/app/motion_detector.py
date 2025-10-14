d790ef8c9eeabc34d1da2327a7bf571a0eb1381e
noname2785 <shelby02225@gmail.com>
2025-10-14 20:12:09 +0200

    Create motion_detector.py
    Add motion_detector.py module (stub)

---
+++ app/app/motion_detector.py
@@ -0,0 +1,8 @@
+# motion_detector.py
+# Planned module for implementing motion detection algorithms in the Smart Surveillance System.
+
+def detect_motion(frame):
+    # Algorithm will be implemented in the next release.
+    pass
+
+

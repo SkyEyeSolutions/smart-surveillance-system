9fb83514bea78b3d832fb789399120517206d261
Pion Tech FZE <shelby02225@gmail.com>
2025-10-20 12:01:15 +0200

    Update logger.py
    ask Andy to check

--- app/app/logger.py
+++ app/app/logger.py
@@ -1,1 +1,14 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+def setup_logger():
+    logger = logging.getLogger("SmartSurveillance")
+    logger.setLevel(logging.INFO)
+
+    handler = RotatingFileHandler("logs/system.log", maxBytes=2_000_000, backupCount=3)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
